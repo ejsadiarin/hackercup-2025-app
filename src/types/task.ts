072423f8b4863021f6caf9279a4f7b4e3fa497dd
ejsadiarin <ejs@ejsadiarin.com>
2025-08-16 02:11:11 +0800

    feat: create task type

---
+++ src/types/task.ts
@@ -0,0 +1,9 @@
+export type Task = {
+  id: number;
+  created_at: string;
+  due_date: string | null;
+  title: string;
+  status: 'todo' | 'inprogress' | 'done';
+  task_type: string | null;
+  user_id: string;
+};
