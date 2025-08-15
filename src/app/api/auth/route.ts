7b4d52ded1e8aa84777a41f0a22cfb0f396abdf5
Ej Sadiarin <106421149+ejsadiarin@users.noreply.github.com>
2025-08-16 00:28:37 +0800

    feat: add tasks crud (#18)
    * feat: add google auth and supabase client
    
    * feat: supabase auth
    
    * fix: supabase integration, google oauth
    
    * feat: dockerize for deployment
    
    * feat: add tasks CRUD

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,19 @@
+
+import { NextRequest, NextResponse } from 'next/server';
+import { createClient } from '@/utils/supabase/server';
+
+export async function GET(req: NextRequest) {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: `${req.nextUrl.origin}/api/auth/callback`
+    }
+  });
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.redirect(data.url);
+}
