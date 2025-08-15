4077c4e895951db37c3e7400e1c1b1e48dbf3c43
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-15 23:23:26 +0900

    feat: remove tasks =

--- src/features/tasks/components/task-lists.tsx
+++ src/features/tasks/components/task-lists.tsx
@@ -1,15 +1,21 @@
 import { Checkbox } from "@/components/ui/checkbox";
 
 type taskListProps = {
-  task?: String;
+  task?: string;
+  onRemove?: () => void;
 };
 
-export default function TaskList({ task }: taskListProps) {
+export default function TaskList({ task, onRemove }: taskListProps) {
   return (
-    <>
-      <div className="flex items-center gap-2">
-        <Checkbox disabled /> <p>{task || "Task List "}</p>
-      </div>
-    </>
+    <div className="flex items-center gap-2">
+      <Checkbox disabled />
+      <p>{task || "Task List"}</p>
+      <button
+        onClick={onRemove}
+        className="ml-auto text-[#E23D29] hover:text-[#e23b29d8] text-2xl"
+      >
+        ×
+      </button>
+    </div>
   );
 }
--- src/features/tasks/container/tasks-tab.tsx
+++ src/features/tasks/container/tasks-tab.tsx
@@ -21,6 +21,10 @@ export default function TasksTab() {
     }
   };
 
+  const handleRemoveTask = (index: number) => {
+    setNewTasks((prev) => prev.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="flex flex-col gap-4 pb-12">
       <div className="flex bg-white z-10 py-2">
@@ -28,7 +32,11 @@ export default function TasksTab() {
       </div>
 
       {tasks.map((task, index) => (
-        <TaskList key={index} task={task} />
+        <TaskList
+          key={index}
+          task={task}
+          onRemove={() => handleRemoveTask(index)}
+        />
       ))}
 
       <div className="flex items-center gap-2">
@@ -52,6 +60,7 @@ export default function TasksTab() {
           className="border-b-2 border-black outline-none px-2 py-1"
         />
       </div>
+
       {/* Start My Day Button at the bottom */}
       <div className="mt-72">
         <button className="flex w-full justify-center border-2 bg-[#A600A9] outline-none text-white px-4 py-2 rounded-lg font-bold">
