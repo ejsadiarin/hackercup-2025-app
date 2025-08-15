887339dda604842484330c9a4f8bc7c45407da32
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-08-15 22:11:20 +0800

    feat: draggable schedule time picker

---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,11 @@
+import DayPlanner from "@/features/calendar/component/day-planner";
+
+export default function SchedulePage() {
+  return (
+    <>
+      <main>
+        <DayPlanner />
+      </main>
+    </>
+  );
+}
---
+++ src/features/calendar/types/types.ts
@@ -0,0 +1,7 @@
+export type Task = {
+  id: string;
+  title: string;
+  start: string;
+  end: string;
+  color: string;
+};
