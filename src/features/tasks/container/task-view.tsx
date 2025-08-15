946c7d10645e3fb49e6b396739c89575187f3a55
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-08-16 03:47:29 +0800

    fix: made task view accpet children

--- src/features/tasks/container/task-view.tsx
+++ src/features/tasks/container/task-view.tsx
@@ -1,21 +1,30 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 
-export default function TaskView() {
+export default function TaskView({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <>
       <main className="min-h-screen">
         <section className="px-8 py-12">
           <span className="flex items-center gap-2">
             <Checkbox disabled />
-            <h2 className="text-xl">Schedule Dentist Appointment</h2>
+            <h2 className="text-2xl font-semibold">
+              Schedule Dentist Appointment
+            </h2>
           </span>
         </section>
         {/* TEMP */}
-        <section className="h-[576px] bg-slate-600"></section>
+        {children}
         <section className="px-8 py-12">
-          <Button className="block max-w-96 w-full mx-auto text-xl h-fit font-medium">
-            Complete
+          <Button
+            variant={"violet"}
+            className="block max-w-96 w-full mx-auto text-xl h-fit font-semibold"
+          >
+            Complete Task
           </Button>
         </section>
       </main>
