16d3faadfc9f38cad1c0ac2f352a05df730e8b1b
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-16 11:27:47 +0900

    feat: study == focus mode

--- src/features/tasks/container/task-view.tsx
+++ src/features/tasks/container/task-view.tsx
@@ -60,8 +60,7 @@ export default function TaskView() {
       >
         {tasks[currentIndex].task_type === "bili" && <GroceryTab />}
         {tasks[currentIndex].task_type === "appointment" && <EmailTab />}
-        {tasks[currentIndex].task_type === "punta" && <FocusMode />}
-        {tasks[currentIndex].task_type === "study" && <Timer />}
+        {tasks[currentIndex].task_type === "study" && <FocusMode />}
       </IndivTask>
 
       {/* Right Arrow */}
@@ -78,8 +77,9 @@ export default function TaskView() {
         {tasks.map((_, index) => (
           <span
             key={index}
-            className={`w-3 h-3 rounded-full transition-all ${index === currentIndex ? "bg-[#A600A9] scale-110" : "bg-gray-300"
-              }`}
+            className={`w-3 h-3 rounded-full transition-all ${
+              index === currentIndex ? "bg-[#A600A9] scale-110" : "bg-gray-300"
+            }`}
           />
         ))}
       </div>
